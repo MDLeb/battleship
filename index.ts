5bbd9b5617625b9795328fbdd909cca184bd770a
Mariya <m.lebadina@freeplay.io>
2024-02-26 16:23:56 +0300

    feat: fix :dev :prod scripts

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import { httpServer } from "./src/http_server/index.ts";
-import {newWSConnection} from './src/websocket/ws.ts'
+import { httpServer } from "./src/http_server/index";
+import {newWSConnection} from './src/websocket/ws'
 const HTTP_PORT = 8181;
 
 console.log(`Start static http server on the ${HTTP_PORT} port!`);
